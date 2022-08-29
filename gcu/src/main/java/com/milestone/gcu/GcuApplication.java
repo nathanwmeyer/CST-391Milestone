e7c74a7ca5625ec2bffc73acc3f130bb2312d2cd
nathanwmeyer <84550580+nathanwmeyer@users.noreply.github.com>
2022-08-28 19:14:20 -0700

    Cloud Update

--- gcu/src/main/java/com/milestone/gcu/GcuApplication.java
+++ gcu/src/main/java/com/milestone/gcu/GcuApplication.java
@@ -2,9 +2,10 @@ package com.milestone.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GcuApplication {
+public class GcuApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcuApplication.class, args);
